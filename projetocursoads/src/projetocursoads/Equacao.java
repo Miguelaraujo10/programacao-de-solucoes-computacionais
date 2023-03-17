9d6098ffafe21efc572100fa5e79c8867f578839
Miguel Araújo <92738573+Miguelaraujo10@users.noreply.github.com>
2023-03-17 19:23:40 -0300

    Update Equacao.java

--- projetocursoads/src/projetocursoads/Equacao.java
+++ projetocursoads/src/projetocursoads/Equacao.java
@@ -1,37 +1,39 @@
 package projetocursoads;
 
 
-    public class Equacao {
-		private double a ;
-		private double b ;
-		private double c ;
-		private double delta ;
-		private double x1 ;
-		private double x2; 
+      public class Equacao {
+	 private double a ;
+	 private double b ;
+	 private double c ;
+	 private double delta ;
+	 private double x1 ;
+	 private double x2;  
+	
+    
+  
+	
+	
+	public double calculaDelta (double a, double b, double c) {
 		
-	    
-	    this.a = a;
-	    this.b = b;
-	    this.c = c;
+		delta = Math.pow(b, 2) - 4* a *c;
+		return  delta;
+	}
+	
+	public double calculaX1(double x1, double x2) {
 		
+		x1 = (-b + Math.sqrt(delta) /   ( 2 * a ) );
 		
-		public void calculaDelta() {
-			delta = Math.pow(b, 2) - 4* a *c;
-			
-			
-		}
+		return x1;
+		 
+	}
+	
+	public double calculaX2() {
 		
-		public void calculaX1() {
-			
-			
-			 
-		}
+		x1 = (-b - Math.sqrt(delta) /   ( 2 * a ) );
 		
-		public void calculaX2() {
-			
-			
-		}
-
+		return x2;
+   
 	}
 
+}
 
