a23d3e1a026c7927348e232d41ff2fedd939c810
mgaraujo <miguelaraujo.bar@gmail.com>
2023-03-31 20:32:30 -0300

    faculdadae

--- src/projetocursoads/Dolar.java
+++ src/projetocursoads/Dolar.java
@@ -8,6 +8,27 @@ public class Dolar {
 
 	public static void main(String[] args) {
 		
+
+	     //Joption
+		   
+		   double cash1 , muchc1, total1;
+		   
+	     
+	     cash1 = Double.parseDouble(JOptionPane.showInputDialog("Qual é o atual valor do dólar? "));
+	     
+	     muchc1 = Double.parseDouble(JOptionPane.showInputDialog("Quantos dolares você tem? "));
+	     
+	     double total12 = muchc1 * cash1;
+	     
+	     JOptionPane.showMessageDialog(null, "Atualmente você tem" + total12 + "reais em dolares");
+		
+		
+		
+		
+		
+		
+		
+		//Scanner
      Scanner input = new Scanner (System.in);
      
      System.out.println("Qual é o atual valor do dólar em relação ao real atualmente ? ");
@@ -19,18 +40,11 @@ public class Dolar {
      System.out.println("Atualmente você tem  " + 
      cash * muchc + " reais em dólares"  );
      
+    
+     
+     
      input.close();
 	   System.exit(0);
-     
-     //Joption
-     
-     cash = Double.parseDouble(JOptionPane.showInputDialog("Qual é o atual valor do dólar? "));
-     
-     muchc = Double.parseDouble(JOptionPane.showInputDialog("Quantos dolares você tem? "));
-     
-     double total = muchc * cash;
-     
-     JOptionPane.showMessageDialog(null, "Atualmente você tem" + total + "reais em dolares");
      
 	}
 
--- src/projetocursoads/PrimeiraClasse.java
+++ src/projetocursoads/PrimeiraClasse.java
@@ -1,12 +1,25 @@
 package projetocursoads;
 
+import java.util.Scanner;
+
 public class PrimeiraClasse {
 
 	public static void main(String[] args) {
+	
+		double n1 , n2;
+		Scanner input = new Scanner(System.in);
 		
+		System.out.println("Coloque a sua primeira nota: ");
+		n1 = input.nextDouble();
 		
-		System.out.println("Olá Mundo");
-
+	while (n1 < 10) {
+		
+		n1++;
+		
+		System.out.println("N1 virou " + n1);
+		break;
+	}
+	
 	}
 
 }
